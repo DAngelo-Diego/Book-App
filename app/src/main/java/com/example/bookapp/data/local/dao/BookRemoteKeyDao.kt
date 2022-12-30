50ee91667a89279a275712037e371576c6d67639
DAngelo-Diego <dangelodiego967@gmail.com>
2022-12-30 19:08:43 -0300

    Remote Mediator Starting

--- app/src/main/java/com/example/bookapp/data/local/dao/BookRemoteKeyDao.kt
+++ app/src/main/java/com/example/bookapp/data/local/dao/BookRemoteKeyDao.kt
@@ -4,18 +4,18 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.bookapp.domain.model.BookRemoteKey
+import com.example.bookapp.domain.model.BookRemoteKeys
 
 @Dao
 interface BookRemoteKeyDao {
 
-    @Query("SELECT * FROM book_remote_key_table WHERE id = :id")
-    suspend fun getRemoteKey(id: Int): BookRemoteKey?
+    @Query("SELECT * FROM book_remote_keys_table WHERE id = :id")
+    suspend fun getRemoteKeys(id: Int): BookRemoteKeys?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addAllRemoteKeys(bookRemoteKeys: List<BookRemoteKey>)
+    suspend fun addAllRemoteKeys(bookRemoteKeys: List<BookRemoteKeys>)
 
-    @Query("DELETE FROM book_remote_key_table")
+    @Query("DELETE FROM book_remote_keys_table")
     suspend fun deleteAllRemoteKeys()
 
 }
--- app/src/main/java/com/example/bookapp/util/Constants.kt
+++ app/src/main/java/com/example/bookapp/util/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
 
     const val BOOK_DATABASE = "book_database"
     const val BOOK_DATABASE_TABLE = "book_table"
-    const val BOOK_REMOTE_KEY_DATABASE_TABLE = "book_remote_key_table"
+    const val BOOK_REMOTE_KEYS_DATABASE_TABLE = "book_remote_keys_table"
 
     const val PREFERENCES_NAME = "boruto_preferences"
     const val PREFERENCES_KEY = "on_boarding_completed"
