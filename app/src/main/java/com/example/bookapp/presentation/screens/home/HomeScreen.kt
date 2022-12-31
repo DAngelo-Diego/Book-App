5fa1f1f0be07370576cf1d4b86f10fd2fbf49512
DAngelo-Diego <dangelodiego967@gmail.com>
2022-12-31 09:06:00 -0300

    BookUseCase Implementation and HomeViewModel created

--- app/src/main/java/com/example/bookapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/bookapp/presentation/screens/home/HomeScreen.kt
@@ -2,9 +2,15 @@ package com.example.bookapp.presentation.screens.home
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    homeViewModel: HomeViewModel = hiltViewModel()
+) {
+
+    val allBooks =homeViewModel.getAllBooks.collectAsLazyPagingItems()
 
     Scaffold(
         topBar = {
